921359781ef0a2a8b88c825e14bf11d0ee4d5531
Kamil Fiks <fikskamil@gmail.com>
2019-02-18 11:54:04 +0100

    added restart button

--- colorGame.js
+++ colorGame.js
@@ -4,7 +4,21 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
-colorDisplay.textContent =pickedColor;
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function (){
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "#232323";
+
+});
+
+
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i ++){
     //add initial colors to squares
@@ -18,6 +32,7 @@ for (var i = 0; i < squares.length; i ++){
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
+            resetButton.textContent = "Play again?";
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again";
@@ -46,5 +61,4 @@ function randomColor() {
     var g = Math.floor(Math.random() * 256)
     var b = Math.floor(Math.random() * 256)
     return "rgb(" + r + ", " + g +", " + b + ")";
-    
 }
